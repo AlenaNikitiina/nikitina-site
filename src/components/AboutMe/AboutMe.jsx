7d12e337720ce4591f08f84fb20337da1bf59388
Alena Nikitina <alena.tkachenk@mail.ru>
2024-06-27 10:25:29 +0300

    add meta for yandex webmaster and google

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
         <div className='about-me__w'>
           <div className='about-me__info'>
             <h3 className='about-me__name'>Алёна</h3>
-            <p className='about-me__profession'>Junior Full-stack разработчик</p>
+            <p className='about-me__profession'>Junior Full-stack web developer</p>
             <p className='about-me__text'>Я родилась в Московской области. Закончила Первый Московский государственный медицинский университет им. И. М. Сеченова и несколько лет работала врачом анестезиологом-реаниматологом. После решила эммигрировать и поняла, что мне нужна профессия, в которой я не буду привязанной к месту. Тем более ранее параллельно с основной раюотой я пробовала профессии связанные с дизайном и программированием. Теперь полностью посвящаю себя web разработке. В данный момент завершила один коммерческий сайт и приступаю к следующему. Могу и вам создать!</p>
             <a className='about-me__link link' href='https://github.com/AlenaNikitiina' target='blank'>Github</a>
           </div>
